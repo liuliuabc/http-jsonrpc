5d34758fea0ec8a124e3a8a553d4caf77719511a
lyh <lyh@YNSH100367.local>
2019-10-17 11:02:59 +0800

    fix export

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
-export * from "./HttpError";
-export * from "./HttpRouter";
+export {default as HttpError} from "./HttpError";
+export {default as HttpRouter} from "./HttpRouter";
 export * from "./HttpMethod";
-export * from "./HttpRpcClient";
-export * from "./HttpRpcServer";
+export {default as HttpRpcClient} from "./HttpRpcClient";
+export {default as HttpRpcServer} from "./HttpRpcServer";
 export * from "./Model";
 export * from "./util";
